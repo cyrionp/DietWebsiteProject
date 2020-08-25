0ba03c56751317d79102f007f3dd813338a38dde
cyrionpow <qadrrdrmz@gmail.com>
2020-08-26 02:02:58 +0300

    summary

--- bmrCalculation.js
+++ bmrCalculation.js
@@ -7,14 +7,14 @@ var calculate=document.querySelector("#calculate");
 
     calculate.onclick=function(){
         if(sex.value=="male"){
-         var bmrCalc=65+(6.23*weight.value)+(12.7*inch.value)-(6.8*age.value);
+         var bmrCalc=(65+(6.23*weight.value)+(12.7*inch.value)-(6.8*age.value))/100;
          var result=bmrCalc*Number(activity.value);
-         console.log(result);
+         document.getElementById("resultText").innerHTML=result.toFixed(2);
         }
 
         else if(sex.value=="female"){
-         var bmrCalc=655+(4.35*weight.value)+(4.7*inch.value)-(4.7*age.value);
+         var bmrCalc=(655+(4.35*weight.value)+(4.7*inch.value)-(4.7*age.value))/100;
          var result=bmrCalc*Number(activity.value);
-         console.log(result);
+         document.getElementById("resultText").innerHTML=result.toFixed(2);
         }
-    }
+	}
